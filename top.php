9eaf0189f34699d149fb403c407b0c9f9a7f7448
SFidan <kaanf@live.co.uk>
2019-05-28 18:38:23 +0100

    added favicon

--- top.php
+++ top.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Tape Agent</title>
+    <link rel='favicon' type='image/x-icon' href='/favicon.ico' />
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
